72cfaa5e76db5eee399bf2aed38d60c1caee5d9f
Akhil <akhilpailwan7143@gmail.com>
2026-02-15 22:32:26 +0530

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/valentine-wishes/', // 👈 change this
+  base: '/valentine-wishes/', // 👈 VERY IMPORTANT
 })
